d0e051dd43a84003b8e2476805d65e154f4aaef2
Pallavi-varekar <pallavivarekar4@gmail.com>
2022-08-13 16:39:50 +0530

    added:modified employee wage computation programs

--- src/employee.java
+++ src/employee.java
@@ -1,5 +1,17 @@
 public class employee {
     public static void main(String[] args) {
         System.out.println("welcome to employee wage computation programs");
+        int wagePerHour = 20;
+        int fullDayHour = 8;
+        int dailyWage = 0;
+        int employeeCheck = (int) Math.floor(Math.random() * 10) % 2;
+        if(employeeCheck == 1) {
+            dailyWage = fullDayHour * wagePerHour;
+            System.out.println("Employee is present");
+        } else {
+            System.out.println("Employee is absent");
+        }
+        System.out.println("Employee total wage is: " + dailyWage);
     }
-}
+    }
+
