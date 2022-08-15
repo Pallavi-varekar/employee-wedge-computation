a175e6cfe00480f97c05d24f9ce1c28d7d5d3674
Pallavi-varekar <pallavivarekar4@gmail.com>
2022-08-15 20:21:53 +0530

    modified:lgic forpart time calculation

--- src/employee.java
+++ src/employee.java
@@ -1,5 +1,23 @@
 public class employee {
     public static void main(String[] args) {
         System.out.println("welcome to employee wage computation programs");
+        int wagePerHour = 20;
+        int fullTimeHour = 8;
+        int partTimeHour = 4;
+        int dailyWage = 0;
+
+        int employeeCheck = (int) (Math.random() * ((2 + 0) + 1));
+
+        if (employeeCheck == 2) {
+            dailyWage = fullTimeHour * wagePerHour;
+            System.out.println("Employee is present full time");
+        } else if (employeeCheck == 1) {
+            dailyWage = partTimeHour * wagePerHour;
+            System.out.println("Employee is present half time");
+        } else {
+            System.out.println("Employee is absent");
+        }
+        System.out.println("Employee total wage is: " + dailyWage);
     }
-}
+    }
+
